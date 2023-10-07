ebffe54d086dc052d0918455dee272e66883ff10
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2023-10-08 00:43:50 +0300

    .

---
+++ BookStoreServer.WebApi/Controllers/BooksController.cs
@@ -0,0 +1,64 @@
+﻿using BookStoreServer.WebApi.Dtos;
+using BookStoreServer.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace BookStoreServer.WebApi.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class BooksController : ControllerBase
+{
+    private static List<Book> books = new()
+    {
+
+    };
+
+    public BooksController()
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            var book =new Book()
+            {
+                Id = i+1,
+                Title = "kitap" + (i+1),
+                Author = "yazar" + (i+1),
+                Summary = "",
+                CoverImageUrl = "https://m.media-amazon.com/images/I/71Qde+ZerdL._AC_UF1000,1000_QL80_.jpg",
+                CreatedAt = DateTime.Now,
+                IsActive = true,
+                ISBN = "34567876543234",
+                Price = 5*(i+1),
+                Quantity = i+1,
+            };
+
+            books.Add(book);
+
+        }
+    }
+
+
+
+    [HttpGet("{pageNumber}/{pageSize}")]
+    public IActionResult GetAll(int pageNumber,int pageSize)
+    {
+        ResponseDto<List<Book>> response = new();
+
+
+        response.Data= books
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+        response.PageNumber= pageNumber;
+        response.PageSize= pageSize;
+        response.TotalPageCount = (int)Math.Ceiling(books.Count / (double)pageSize);
+        response.IsFirstPage = pageNumber == 1;
+        response.IsLastPage = pageNumber == response.TotalPageCount;
+
+
+
+
+        return Ok(books);
+    }
+
+}
